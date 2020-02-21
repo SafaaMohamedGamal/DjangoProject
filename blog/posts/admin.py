04192f8d591fea94c8943833f8b7e9fe40b9e487
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-02-21 21:15:56 +0200

    solving problems in showposts.html

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import poststable, comments, replys, likes
 # Register your models here.
+admin.site.register(poststable)
+admin.site.register(comments)
+admin.site.register(replys)
+admin.site.register(likes)
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import comments, replys
 
-# Create your views here.
+def showPosts(request):
+	return render(request, 'showPosts.html')
+
+def commentsReplys(request):
+	allcomments = comments.objects.filter(postId=1).order_by('commentTime')
+	allreplys = replys.objects.filter(postId=1).order_by('replyTime')
+	# context = {'commentReply':(allcomments , allreplys)}
+	context = {'comments': allcomments, 'replys': allreplys }
+	return render(request, 'showPosts.html', context)
