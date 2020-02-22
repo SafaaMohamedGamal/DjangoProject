7dabfb71d274805faeb4d15a36b6e2b566587304
hager <hgrreda@gmail.com>
2020-02-22 21:32:43 +0200

    update models

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', views.commentsReplys),
+    url(r'^$', views.showPosts),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -5,6 +5,27 @@ from .models import comments, replys
 def showPosts(request):
 	return render(request, 'showPosts.html')
 
+def showPosts(request):
+	all_posts = Post.objects.all()
+	context = {'all_posts':all_posts }
+	return render(request ,'showPosts.html' ,context)
+
+
+def subscribeCategory(request,user_num,cat_num):
+	instance = categories_users_id(categories_id=cat_num, user_id=user_num)
+	if(subscribeCategory):
+		instance.save()
+	else:
+		instanse.delete()
+
+def allCategoryPosts(request,cat_num):
+	  queryset = Post.objects.filter(category_type=cat_num).order_by('-time_created')
+
+def searchForPost(string):
+	queryset = Post.objects.filter(title=string) | Post.objects.filter(tag_post = string)
+	ordered_query = queryset.order_by('-time_created')
+	
+
 def commentsReplys(request):
 	allcomments = comments.objects.filter(postId=1).order_by('commentTime')
 	allreplys = replys.objects.filter(postId=1).order_by('replyTime')
