af23d63a1ae09df0c5da8bb91a5a24ba56aa3924
Ali Ahmed Gomaa <alyahmedgomaa@gmail.com>
2020-02-24 17:30:18 +0200

    Some Modification at Email part For Authentication

--- blog/authentication/views.py
+++ blog/authentication/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .forms import RegisterForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid:
-            form.save()
-            return redirect('home')
+def signup(response):
+    if response.method == "POST":
+	    form = RegisterForm(response.POST)
+	    if form.is_valid():
+	        form.save()
+
+	        return redirect("home")
     else:
-        form = UserCreationForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'registration/sign_up.html', context)
+	    form = RegisterForm()
+
+    return render(response, "registration/sign_up.html", {"form":form})
 
 
