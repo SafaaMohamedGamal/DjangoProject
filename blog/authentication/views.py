f84aa431fe94d0acbde0491eb34ecc5d8843cb0b
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-03-03 06:57:52 +0200

    fixing bugs

--- blog/authentication/views.py
+++ blog/authentication/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .forms import RegisterForm
-from templates import admin
+# from templates import admin
 
 
 # Create your views here.
